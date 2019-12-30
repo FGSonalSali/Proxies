ae803cd3e85a437d236e9ae4ccc309c549bcd21b
Sonal <sonal.sali@PG00E616.freshgravity.com>
2019-12-30 16:17:49 +0530

    new1

--- trial.sh
+++ trial.sh
@@ -4,21 +4,21 @@ for f in *; do
   cd "$f"
   pwd
   pass="Secret123"
-  var=$(apigeetool listdeployments -L "http://192.168.2.40:8080" -u trial@apigee.com -o trial -n $f -p $pass)
-echo $var
+#  var=$(apigeetool listdeployments -L "http://192.168.2.40:8080" -u trial@apigee.com -o trial -n $f -p $pass)
+#echo $var
 
-IFS=' ' read -a arr<<< "${var}"
-revision_stable=${arr[2]}
+#IFS=' ' read -a arr<<< "${var}"
+#revision_stable=${arr[2]}
 
-echo $revision_stable
+#echo $revision_stable
 
   new_deployment=$(apigeetool deployproxy -L "http://192.168.2.40:8080" -u trial@apigee.com -o trial -e test -n $f -d . -p $pass -k)
   echo $new_deployment
-  cd tests/
-  for t in *;do
-	  jmeter.sh -n -t $t -l $f.log
-  done
-  cd ..
+  #cd tests/
+  #for t in *;do
+#	  jmeter.sh -n -t $t -l $f.log
+  #done
+  #cd ..
   cd ..
 
 #cd ${JMETER_HOME}/bin
